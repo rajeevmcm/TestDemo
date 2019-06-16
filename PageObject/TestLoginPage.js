ceba33c9544f04c8fbd3f61849c4464298362a5e
Rajeev Kumar <Rajeev.Kumar@sqs.com>
2019-06-16 21:45:46 +0530

    update file

--- PageObject/TestLoginPage.js
+++ PageObject/TestLoginPage.js
@@ -11,9 +11,9 @@ var DocumentationPage = function () {
 		browser.sleep(2000);
     };
     this.enterValue = function(){
-        firstTxt.sendKeys(1);
+        firstTxt.sendKeys(2);
         browser.sleep(100);
-        secondTxt.sendKeys(2);
+        secondTxt.sendKeys(1);
         browser.sleep(100);
         goBtn.click();
         browser.sleep(100);
